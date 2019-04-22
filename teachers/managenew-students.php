0e19924f1caa4e828fab51bc6f855bee5b091f0c
yongpresido <segunemma2003@gmail.com>
2019-04-23 00:10:23 +0100

    test

--- teachers/managenew-students.php
+++ teachers/managenew-students.php
@@ -29,14 +29,14 @@ else{
 <link href="css/style.css" rel='stylesheet' type='text/css' />
 <link rel="stylesheet" href="css/morris.css" type="text/css"/>
 <!-- Graph CSS -->
-<link href="css/font-awesome.css" rel="stylesheet">
+<link href="../admin/css/font-awesome.css" rel="stylesheet">
 <!-- jQuery -->
-<script src="js/jquery-2.1.4.min.js"></script>
+<script src="../admin/js/jquery-2.1.4.min.js"></script>
 <!-- //jQuery -->
 <!-- tables -->
-<link rel="stylesheet" type="text/css" href="css/table-style.css" />
-<link rel="stylesheet" type="text/css" href="css/basictable.css" />
-<script type="text/javascript" src="js/jquery.basictable.min.js"></script>
+<link rel="stylesheet" type="text/css" href="../admin/css/table-style.css" />
+<link rel="stylesheet" type="text/css" href="../admin/css/basictable.css" />
+<script type="text/javascript" src="../admin/js/jquery.basictable.min.js"></script>
 <script type="text/javascript">
     $(document).ready(function() {
       $('#table').basictable();
@@ -68,7 +68,7 @@ else{
 <link href='//fonts.googleapis.com/css?family=Roboto:700,500,300,100italic,100,400' rel='stylesheet' type='text/css'/>
 <link href='//fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
 <!-- lined-icons -->
-<link rel="stylesheet" href="css/icon-font.min.css" type='text/css' />
+<link rel="stylesheet" href="../admin/css/icon-font.min.css" type='text/css' />
 <!-- //lined-icons -->
 </head>
 <body>
