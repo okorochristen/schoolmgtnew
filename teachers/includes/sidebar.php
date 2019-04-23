9df2067122253a469b032cf848274d5647df6c1d
yongpresido <segunemma2003@gmail.com>
2019-04-23 14:52:56 +0100

    checkers

--- teachers/includes/sidebar.php
+++ teachers/includes/sidebar.php
@@ -1,3 +1,25 @@
+<?php
+// DB credentials.
+$host = "localhost";
+$username = "root";
+$password = "password";
+$database = "scho";
+
+// $host = "localhost";
+// $username = "beginnersbasicsc_mgt";
+// $password = "ilovedaniel";
+// $database = "beginnersbasicsc_mgt";
+// Establish database connection.
+
+// Establish database connection.
+$db = new mysqli($host, $username, $password, $database);
+$connect_error = $db->connect_error;
+if ($connect_error != null) {
+	echo "database connection error ".$connect_error;
+	exit;
+}
+?>
+
 <aside>
           <div id="sidebar"  class="nav-collapse ">
               <!-- sidebar menu start-->
@@ -6,12 +28,12 @@
               	  <p class="centered"><a href="profile.php"><img src="../images/1a.jpg" class="img-circle" width="60"></a></p>
                    <?php
                       $email = $_SESSION['login'];
-                    //   $query= $db->prepare("SELECT name from teachers where emailid = ?");
-                    //   $query->bind_param('s',$email);
-                    //   $query->execute();
-                    //   $query->bind_result($staffName);
-                    //   $query->fetch();
-                    //   $query->close();
+                      $query= $db->prepare("SELECT name from teachers where emailid = ?");
+                      $query->bind_param('s',$email);
+                      $query->execute();
+                      $query->bind_result($staffName);
+                      $query->fetch();
+                      $query->close();
 
                   ?>
               	  <h5 class="centered"><?php echo htmlentities($staffName); ?></h5>
