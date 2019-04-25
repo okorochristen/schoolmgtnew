bb69211e52817632708bafbf5dfc9d09620b7645
uzohemmanuel55 <echezo55@gmail.com>
2019-04-25 12:56:07 -0700

    thirdcommit

--- teachers/includes/header.php
+++ teachers/includes/header.php
@@ -3,11 +3,12 @@
                   <div class="fa fa-bars tooltips" data-placement="right" data-original-title="Toggle Navigation"></div>
               </div>
             <!--logo start-->
-            <a href="index.php" class="logo"><b>Dashboard<b></a>
+            <a href="index.php" class="logo"><b>Dashboard</b></a>
                 
             
             	<ul class="navbar-nav ml-auto">
                     <li><a class="btn btn-outline-white btn-md" href="logout.php">Logout</a></li>
             	</ul>
+
             
         </header>
